acc4c8e64c6b82f40a138a526887c152851fe6fc
EuKennedy <kennedy.rodrigues1104@gmail.com>
2023-09-13 17:24:52 -0300

    Api connected to javascript and working. 70% of the js code has already been done.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
 // functions
@@ -20,12 +20,22 @@ const getWeatherData = async (city) => {
     const res = await fetch(apiWeatherURL)
     const data = await res.json();
 
-    console.log(data)
+    return data
 };
 
 
 const showWeatherData = async (city) => {
-    getWeatherData(city);
+   const data = await getWeatherData(city);
+
+   cityElement.innerText = data.name;
+   tempElement.innerText = parseInt(data.main.temp);
+   descElement.innerText = data.weather[0].description;
+   weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+   
+   countryElement.setAttribute("src", apiCountryURL + data.sys.country + ".png");
+   humidityElement.innerText = `${data.main.humidity}%`;
+   windElement.innerText = `${data.wind.speed}km/h`
+    
 };
 
 
@@ -35,4 +45,4 @@ searchBtn.addEventListener("click", (e)=> {
     const city = cityInput.value;
     showWeatherData(city);
     
-});
+}); 
