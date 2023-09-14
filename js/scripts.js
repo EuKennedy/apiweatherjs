83ebee0833b7d991361a771ed2420fe5c4af98f4
EuKennedy <kennedy.rodrigues1104@gmail.com>
2023-09-14 15:27:12 -0300

    I remove my api key from js because my security.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const apiKey = "b5d9b65689d4af6592fd0dcdfc1d96ee";
+const apiKey = "";
 const apiCountryURL = "https://www.countryflagicons.com/FLAT/64/";
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
